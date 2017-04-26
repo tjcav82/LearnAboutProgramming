d1791f6f953394238fe11b004cc5bf4c1a3c42dd
Amari Vaughn <redsage23@gmail.com>
2017-04-26 17:02:14 -0400

    Update GameFragment.java

--- app/src/main/java/com/tjcav82gmail/learnaboutprogramming/GameFragment.java
+++ app/src/main/java/com/tjcav82gmail/learnaboutprogramming/GameFragment.java
@@ -11,13 +11,46 @@ import android.view.ViewGroup;
  * Created by tjcav_000 on 3/29/2017.
  */
 
-public class GameFragment extends Fragment{
+public class GameFragment extends Fragment implements View.OnClickListener{
    View myView;
+    public QuestionsClass questions=new QuestionsClass();
+    public Question currentquestion;
+    RadioButton a;
+    RadioButton b;
+    RadioButton c;
+    RadioButton d;
+    TextView qust;
+    int index=0;
     @Nullable
     @Override
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
         myView = inflater.inflate(R.layout.fragment_game,container, false);
+        a=(RadioButton)myView.findViewById(R.id.radioButton);
+        b=(RadioButton)myView.findViewById(R.id.radioButton2);
+        c=(RadioButton)myView.findViewById(R.id.radioButton3);
+        d=(RadioButton)myView.findViewById(R.id.radioButton4);
         return myView;
+    }
+    public Question getQuestion(int index, TextView qview, RadioButton a, RadioButton b, RadioButton c, RadioButton d){
+
+        currentquestion=questions.k.get(index);
+        qview.setText(currentquestion.Question);
+        a.setText(currentquestion.a);
+        b.setText(currentquestion.b);
+        c.setText(currentquestion.c);
+        d.setText(currentquestion.d);
+  return currentquestion;
+    }
+
+    @Override
+    public void onClick(View v) {
+        RadioButton g=(RadioButton)v;
+        if(index<questions.k.size()) {
+            if (currentquestion.ans_check(g.getText().toString())) {
+                index++; //go to next question
+                getQuestion(index, qust, a, b, c, d);
+            }
+        }
     }
 }
